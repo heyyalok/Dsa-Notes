7e5524f18e68fd70752e123e8b2b6b16e2d30d12
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-08-31 18:58:15 +0530

    Update preOrderTraversalWithoutRecursion.java

--- Trees/preOrderTraversalWithoutRecursion.java
+++ Trees/preOrderTraversalWithoutRecursion.java
@@ -1,26 +1,28 @@
-import java.util.Stack;
-public class Solution
-{
-  public static void preOrderWithoutRecursion(BinaryTreeNode<Integer>root)
-  {
-    if (root == null)
-    {
-      return;
-    }
-    Stack<BinaryTreeNode<Integer>> s = new Stack<BinaryTreeNode<Integer>>();
-    s.add(root);
-    while (!s.isEmpty())
-    {
-      BinaryTreeNode<Integer>frontNode = s.pop();
-      System.out.print(frontNode.data);
-      if (frontNode.right != null)
-      {
-        s.push(frontNode.right);
-      }
-      if (frontNode.left!=null)
-      {
-        s.push(frontNode.right);
-      }
-    }
-  }
+public class Solution {
+
+	public static void preOrder(BinaryTreeNode<Integer> root) {
+		//Your code goes here
+        if (root == null)
+        {
+            return ;
+        }
+        
+        Stack<BinaryTreeNode<Integer>> s = new Stack<BinaryTreeNode<Integer>>();
+        s.add(root);
+        
+        while (!s.isEmpty())
+        {
+            BinaryTreeNode<Integer>frontNode = s.pop();
+            System.out.print(frontNode.data+" ");
+            
+            if(frontNode.right!=null)
+            {
+                s.push(frontNode.right);
+            }
+            if (frontNode.left!=null)
+            {
+                s.push(frontNode.left);
+            }
+        }
+	}
 }
